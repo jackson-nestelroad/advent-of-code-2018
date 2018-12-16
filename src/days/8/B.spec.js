24e1a72ad4f77a480cf5ebd2597e720729b23665
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2018-12-15 20:01:42 -0600

    Fix typos in testing.

--- src/days/8/B.spec.js
+++ src/days/8/B.spec.js
@@ -1,7 +1,7 @@
 import { expect } from "chai"
 import Solution from "./B"
 
-describe("Day 7 - Puzzle A", () => {
+describe("Day 8 - Puzzle B", () => {
     describe("sumMetadata()", () => {
         it("should return 66 when given example case", () => {
             let input = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
